778f5f8e98f46e4ff93326accb291b40377dcde1
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-03-29 10:39:23 +0100

    Update EdiZon scripts

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ cd tmp
 # Update EdiZon_ConfigsAndScripts
 wget https://github.com/WerWolv98/EdiZon_ConfigsAndScripts/archive/master.zip -O EdiZon_ConfigsAndScripts.zip
 unzip EdiZon_ConfigsAndScripts.zip
-cp -rf EdiZon_ConfigsAndScripts-master/Configs/* ../EdiZon/editor/
-cp -rf EdiZon_ConfigsAndScripts-master/Scripts/* ../EdiZon/editor/scripts
+cp -rf EdiZon_CheatsConfigsAndScripts-master/Configs/* ../EdiZon/editor/
+cp -rf EdiZon_CheatsConfigsAndScripts-master/Scripts/* ../EdiZon/editor/scripts
 
 # Update ReiNXToolkit
 wget https://reinx.guide/downloads/ReiNXToolkit.nro -O ../switch/ReiNXToolkit.nro
